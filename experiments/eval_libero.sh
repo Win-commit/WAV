1a26267708f807ff7f65260de7d2a74d88d2e391
Runze Li <lirunze@westlake.edu.cn>
2026-04-17 20:32:07 +0800

    clean

--- experiments/eval_libero.sh
+++ experiments/eval_libero.sh
@@ -2,25 +2,15 @@
 
 gpu=${1:-1}
 
-config_file=configs/ltx_model/libero/action_model_WV_libero.yaml
+config_file=configs/ltx_model/libero/action_model_libero.yaml
 
-output_dir=evaluation_results/libero/Memory_analy/40k/explore_7
+output_dir=""
 
-ckpt_path_goal=/defaultShare/Genie-Envisioner/lrz_training_v2/libero/Phase3_PolicyModel_WV_absolute_eef/2026_02_14_19_00_36/step_40000/diffusion_pytorch_model.safetensors
-ckpt_path_obj=/defaultShare/Genie-Envisioner/lrz_training_v2/libero/Phase3_PolicyModel_WV_absolute_eef/2026_02_14_19_00_36/step_40000/diffusion_pytorch_model.safetensors
-ckpt_path_10=/defaultShare/Genie-Envisioner/lrz_training_v2/libero/Phase3_PolicyModel_WV_absolute_eef/2026_02_14_19_00_36/step_40000/diffusion_pytorch_model.safetensors
-ckpt_path_spa=/defaultShare/Genie-Envisioner/lrz_training_v2/libero/Phase3_PolicyModel_WV_absolute_eef/2026_02_14_19_00_36/step_40000/diffusion_pytorch_model.safetensors
+ckpt_path_goal=/PATH/TO/CKPT
+ckpt_path_obj=/PATH/TO/CKPT
+ckpt_path_10=/PATH/TO/CKPT
+ckpt_path_spa=/PATH/TO/CKPT
 
-
-# EGL_DEVICE_ID=$gpu python  experiments/eval_libero.py \
-#     --config_file $config_file \
-#     --output_dir  $output_dir \
-#     --ckpt_path $ckpt_path_goal \
-#     --exec_step 8 \
-#     --task_suite_name  libero_goal \
-#     --device $gpu \
-#     --num_trails_per_task 50 \
-#     --threshold 20
 
 
 EGL_DEVICE_ID=$gpu python  experiments/eval_libero.py \
@@ -31,26 +21,5 @@ EGL_DEVICE_ID=$gpu python  experiments/eval_libero.py \
     --task_suite_name  libero_10 \
     --device $gpu \
     --num_trails_per_task 50 \
-    --task_ids 0 \
     --threshold 1
 
-
-# EGL_DEVICE_ID=$gpu python  experiments/eval_libero.py \
-#     --config_file $config_file \
-#     --output_dir  $output_dir \
-#     --ckpt_path $ckpt_path_obj \
-#     --exec_step 8 \
-#     --task_suite_name  libero_object \
-#     --device $gpu \
-#     --num_trails_per_task 50 \
-#     --threshold 30
-
-# EGL_DEVICE_ID=$gpu python  experiments/eval_libero.py \
-#     --config_file $config_file \
-#     --output_dir  $output_dir \
-#     --ckpt_path $ckpt_path_spa \
-#     --exec_step 8 \
-#     --task_suite_name  libero_spatial \
-#     --device $gpu \
-#     --num_trails_per_task 50 \
-#     --threshold 30
